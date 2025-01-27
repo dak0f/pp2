15ca9d38160aa4c63b2cfd11d1a0466b2fa5e468
darn <spacestarboom@gmail.com>
2025-01-27 18:15:25 +0500

    your comment

---
+++ pythonlab2/lab2_2.py
@@ -0,0 +1,57 @@
+x = 5 
+x+=3
+print(x)
+
+print(10*5)
+print(10/2)
+
+fruits = ['apple','banana']
+if 'apple' in fruits:
+    print('yes ,apple is a fruit')
+
+if 5!=10:
+    print('5 and 10 are not equal')
+
+if 5==10 or 4==4:
+    print('at least one is correct')
+
+mylist = ['apple' , 'banana' , 'cherry']
+print(mylist[1])
+print(mylist[2])
+
+#false
+#len
+
+mylist = ['apple', 'banana', 'cherry']
+print(mylist[-1])
+fruits = ["apple", "banana", "cherry"]
+print(fruits[1])
+
+mylist = ['apple', 'banana', 'cherry', 'orange', 'kiwi']
+print(mylist[1:4])
+
+mylist = ['apple', 'banana', 'cherry']
+mylist[0] = 'kiwi'
+print(mylist[1])  
+
+fruits = ["apple", "banana", "cherry"]
+fruits[0] ="kiwi"
+
+
+mylist = ['apple', 'banana', 'cherry']
+mylist[1:2] = ['kiwi', 'mango']
+print(mylist[2])
+
+mylist = ['apple', 'banana', 'cherry']
+mylist.insert(0, 'orange')
+print(mylist[1])
+
+fruits = ["apple", "banana", "cherry"]
+fruits.append('orange')
+
+fruits = ["apple", "banana", "cherry"]
+fruits.insert(1, "lemon")
+
+fruits = ["apple", "banana", "cherry"]
+fruits.remove('banana')
+
